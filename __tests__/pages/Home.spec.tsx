fff44806c4c8ccb2ff82ee62ca00efdbc07eba29
Junior de Oliveira <ellismarjr@hotmail.com>
2021-05-25 17:20:53 -0300

    fix: add act when tests change state in the component

--- __tests__/pages/Home.spec.tsx
+++ __tests__/pages/Home.spec.tsx
@@ -7,46 +7,47 @@ import { Home } from '../../src/pages/Home';
 jest.mock('react-native-vector-icons/Feather', () => 'Feather');
 
 describe('Home', () => {
-  it('should be able to render new added skills', () => {
+  it('should be able to render new added skills', async () => {
     const { getByPlaceholderText, getByText } = render(<Home />);
     const inputElement = getByPlaceholderText('New skill');
 
     fireEvent.changeText(inputElement, 'React Native');
-    fireEvent(inputElement, 'submitEditing');
+    await act(() =>  fireEvent(inputElement, 'submitEditing'));
 
     fireEvent.changeText(inputElement, 'Tests in React Native');
-    fireEvent(inputElement, 'submitEditing');
+    await act(() =>  fireEvent(inputElement, 'submitEditing'));
 
     getByText('React Native');
     getByText('Tests in React Native');
   });
 
-  it('should not be able to add an empty skills', () => {
+  it('should not be able to add an empty skills', async () => {
     const { getByPlaceholderText, queryByText } = render(<Home />);
     const inputElement = getByPlaceholderText('New skill');
 
     fireEvent.changeText(inputElement, 'React Native');
-    fireEvent(inputElement, 'submitEditing');
+    await act(() =>  fireEvent(inputElement, 'submitEditing'));
 
     fireEvent.changeText(inputElement, 'React Native');
-    fireEvent(inputElement, 'submitEditing');
+    await act(() =>  fireEvent(inputElement, 'submitEditing'));
 
     expect(queryByText('')).toBeNull();
   });
+
 
   it('should be able to remove skills by "onPress" event', async () => {
     const { getByPlaceholderText, getByText, queryByText, getByTestId } = render(<Home />);
     const inputElement = getByPlaceholderText('New skill');
 
     fireEvent.changeText(inputElement, 'React Native');
-    fireEvent(inputElement, 'submitEditing');
+    await act(() =>  fireEvent(inputElement, 'submitEditing'));
     
     fireEvent.changeText(inputElement, 'Tests in React Native');
-    fireEvent(inputElement, 'submitEditing');
+    await act(() =>  fireEvent(inputElement, 'submitEditing'));
 
     const firstSkill = getByTestId('React Native-handleDeleteSkillButton');
 
-    fireEvent.press(firstSkill);
+    await act(() => fireEvent.press(firstSkill));
 
     expect(queryByText('React Native')).toBeNull();
     getByText('Tests in React Native');
